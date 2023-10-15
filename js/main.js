b9da07d5332883b04c195f608a009c6dbe236290
Jean Michell Canache <134435174+jeancana@users.noreply.github.com>
2023-10-15 16:35:39 -0300

    Practica JavaScritp Nro1

---
+++ js/main.js
@@ -0,0 +1,4 @@
+
+
+console.log('Esta es la SEGUNDA MANERA de escribir Codigo JavaScript - linkeando (haciendo una ruta de acceso) desde Script:src puesto en el index.html -----> RUTA/endpoint : <script src="js / main.js"></script>')
+
